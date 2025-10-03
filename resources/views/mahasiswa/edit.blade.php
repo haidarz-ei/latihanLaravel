8abbd915df8316c9758a53fa1ae139ab991af4c3
haidarz-ei <zzhaidar36@gmail.com>
2025-10-03 17:18:07 +0700

    ubah store ke update di file edit mhs

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-6">
 
-                <form method="POST" action="{{ route('mahasiswa.store') }}" class="space-y-4">
+                <form method="POST" action="{{ route('mahasiswa.update', $mhs->id) }}" class="space-y-4">
                     @csrf
                     @method('PUT')
                     
